f6d334c29182883f11a6c086892a9345d4b862da
unknown <freemanbrown@gmail.com>
2019-05-14 15:10:09 -0400

    added setters and getters to the program

--- Book.java
+++ Book.java
@@ -3,9 +3,21 @@
 
     public class Book{
 
-        int numberofpages;
+        int pages;
         String color;
         boolean ishardCover;
         String publisher;
     
+    public void setcolor(String color){
+        this.color = color;
+    }
+    public String getcolor(){
+        return color;
+    }
+    public void setpages(int pages){
+        this.pages = pages;
+    }
+    public int getpages(){
+        return pages;
+    }
 }
